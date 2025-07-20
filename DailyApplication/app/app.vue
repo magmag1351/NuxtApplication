679295b749279ca8961317ffd808179503e93ec1
magmag <kenta7350u@gmail.com>
2025-07-20 21:58:04 +0900

    vuetify installed

--- DailyApplication/app/app.vue
+++ DailyApplication/app/app.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <v-app>
+    <v-navigation-drawer app permanent width="250" color="surface">
+      <v-list-item class="pa-4">
+        <v-list-item-title class="text-h6 font-weight-bold">
+          <v-icon left>mdi-application</v-icon>
+          DailyApp
+        </v-list-item-title>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-home" title="ホーム" value="home" to="/"></v-list-item>
+        <v-list-item prepend-icon="mdi-account-group" title="ユーザー" value="users" to="/users"></v-list-item>
+        <v-list-item prepend-icon="mdi-cog" title="設定" value="settings"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <NuxtPage />
+    </v-main>
+  </v-app>
 </template>
+
+<style scoped>
+/* 必要に応じてスタイルを追加 */
+</style>
--- DailyApplication/nuxt.config.ts
+++ DailyApplication/nuxt.config.ts
@@ -1,12 +1,31 @@
+import vuetify from 'vite-plugin-vuetify';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-
   modules: [
     '@nuxt/eslint',
-    '@nuxt/icon',
     '@nuxt/fonts',
-    '@nuxt/test-utils'
-  ]
-})
+    '@nuxt/icon',
+    '@nuxt/test-utils',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins?.push(vuetify({ autoImport: true }));
+      });
+    },
+  ],
+  ssr: false,
+  devtools: { enabled: true },
+  build: {
+    transpile: ['vuetify'],
+  },
+  compatibilityDate: '2025-07-15',
+  eslint: {
+    config: {
+      stylistic: {
+        indent: 2,
+        quotes: 'single',
+        semi: true,
+      },
+    },
+  },
+});
